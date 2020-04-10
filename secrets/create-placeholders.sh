d5633b5920d9a499b591c31ba4ef7784f9d059d9
Robert K. Bell <robert.k.bell@gmail.com>
2020-04-10 16:38:14 +1000

    chore: allow generating secrets files with templates

--- secrets/create-placeholders.sh
+++ secrets/create-placeholders.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+# A convenience for creating files that should only exist locally, and
+# therefore aren't tracked in git.
+
 set -e
 
 # The directory this script is in
 # https://stackoverflow.com/q/59895/2014893
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Files that should contain zero linefeeds
 touch "$DIR"/postgres_password
 touch "$DIR"/gatekeeper_db_user_pw
-touch "$DIR"/fetcher.toml
+
+# Files that have structured contents (json / yml / toml / ini etc)
+if [ ! -f "$DIR"/fetcher.toml ]; then cat >"$DIR"/fetcher.toml <<EOF
+[db]
+password = ""
+EOF
+fi
